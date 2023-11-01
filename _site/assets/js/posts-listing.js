c16c5d3d9383fddd672c8f21ec9fc2352d90ac11
Vincent Clemson <vclemson07@gmail.com>
2023-11-01 17:34:11 -0400

    update fonts & listing

--- _site/assets/js/posts-listing.js
+++ _site/assets/js/posts-listing.js
@@ -1,3 +1,7 @@
 navbar = document.querySelector('.navbar')
 navbar.classList.remove('navbar-expand-lg')
 navbar.classList.add('navbar-expand-md')
+title = document.querySelector('.quarto-title-block .title').innerHTML
+document.querySelector('.navbar-container').insertAdjacentHTML(
+  'afterbegin', '<div class="navbar-title-container me-auto"><ul class="navbar-nav navbar-nav-scroll"><li class="nav-item"><span class="nav-link">'+title+'</span></li></ul></div>'
+);
